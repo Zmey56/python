98f9cd6aecdf23d61576e6bc2c6be8f034b5d6c9
Zmey56 <zmey56@gmail.com>
2021-04-14 21:41:38 +0300

    task 5

--- hw5t4.py
+++ hw5t4.py
@@ -6,3 +6,15 @@
 # Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 # При этом английские числительные должны заменяться на русские.
 # Новый блок строк должен записываться в новый текстовый файл.
+
+change = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
+result = []
+
+with open('text_3.txt', 'r') as my_file:
+    for line in my_file:
+        result.append(change.get(line.split()[0]) + ' - ' + line.split()[2])
+
+
+with open('text_3_result.txt', 'w') as my_file_2:
+    for item in result:
+        my_file_2.writelines(item+'\n')
