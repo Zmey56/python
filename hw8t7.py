de2e753dddca518aa98442b305f1ce07686fb329
zmey56 <zmey56@gmail.com>
2021-04-26 11:17:03 +0300

    complex number

--- hw8t7.py
+++ hw8t7.py
@@ -7,21 +7,28 @@
 
 
 class ComplexNumber:
-    def __init__(self, a, b, *args):
-        self.a = a
-        self.b = b
-        self.z = 'a + b * i'
+    def __init__(self, a, b):
+        self.comp_number = (a, b)
 
     def __add__(self, other):
-        print(f'Сумма z1 и z2 равна')
-        return f'z = {self.a + other.a} + {self.b + other.b} * i'
+        result = (self.comp_number[0] + other.comp_number[0], self.comp_number[1] + other.comp_number[1])
+        print(f'The sum of a and b is equal of: {result[0]} + {result[1]}i')
+        return result
 
     def __mul__(self, other):
-        print(f'Произведение z1 и z2 равно')
-        return f'z = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+        a = self.comp_number[0]
+        b = self.comp_number[1]
+        c = other.comp_number[0]
+        d = other.comp_number[1]
+        result = (a*c - b*d), (a*d + c*b)
+        print(f'The multiple of a and b is equal of: {result[0]} + {result[1]}i')
+        return result
 
-    def __str__(self):
-        return f'z = {self.a} + {self.b} * i'
+z_1 = ComplexNumber(1, -2)
+z_2 = ComplexNumber(3, 4)
+print(z_1)
+print(z_1 + z_2)
+print(z_1 * z_2)
 
 
 z_1 = ComplexNumber(1, -2)
